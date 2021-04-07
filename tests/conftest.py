631bdeb760e2a08a41f8c2a1b27eb6a5bb1fe77d
JoryGarrido <jorygarrido@gmail.com>
2021-04-06 22:26:08 -0400

    jory: Adress Second Round of linter

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,3 +10,4 @@ def app():
 @pytest.fixture
 def client(app):
     return app.test_client()
+    
--- tests/test_main.py
+++ tests/test_main.py
@@ -5,13 +5,16 @@ def test_index(app, client):
     res = client.get("/")
     assert res.status_code == 200
 
+
 def test_food(app, client):
     res = client.get("/food")
     assert res.status_code == 200
 
+
 def test_pizza(app, client):
     res = client.get("/food/pizza")
     assert res.status_code == 200
+
 
 def test_404(app, client):
     res = client.get("/food/burgers")
